daaaeea1ae431aadb102180191dd09446715544e
Lapshin Sergey <wslapshin@gmail.com>
2017-10-21 21:06:36 +0300

    big deal

---
+++ application/controllers/page_types/article_relationships.php
@@ -0,0 +1,12 @@
+<?php
+namespace Application\Controller\Article;
+
+use Concrete\Core\Page\Controller\PageTypeController;
+
+class Article extends PageTypeController
+{
+    public function view()
+    {
+        
+    }
+}
