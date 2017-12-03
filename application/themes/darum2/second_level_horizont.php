33a5b4f5454c336607d8b446bfe49a6498674458
Lapshin Sergey <wslapshin@gmail.com>
2017-12-03 22:13:02 +0300

    bugfix

--- application/themes/darum2/second_level_horizont.php
+++ application/themes/darum2/second_level_horizont.php
@@ -222,6 +222,7 @@
                      <div class="page__column-6 page__preview-container">
                          <div class="slick-preview">
                             <?php foreach( $mainCommentedPage as $p ): ?>
+                             <?php var_dump($p); die;?>
                             <?php $pageUtils = new DarumPageUtils($p); ?>
                             <?php $title = $pageUtils->getCategoryTitle(); ?>
                             <div class="intro 
